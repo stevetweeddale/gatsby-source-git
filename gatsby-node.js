bb3c12c4bbcd5d7b3e09173d539be1d468246ab4
Stephen Tweeddale <stephen.tweeddale@computerminds.co.uk>
2018-10-18 17:01:59 +0100

    WIP first initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const Git = require("simple-git/promise");
+const fastGlob = require("fast-glob");
+const fs = require("fs");
+const { createFileNode } = require("gatsby-source-filesystem/create-file-node");
+
+async function isAlreadyCloned(remote, path) {
+  const existingRemote = await Git(path).raw(['config', '--get', 'remote.origin.url']);
+  return existingRemote.trim() == remote.trim();
+}
+
+async function getRepo(path, remote, branch) {
+  // If the directory doesn't exist or is empty, clone
+  if (!fs.existsSync(path) || fs.readdirSync(path).length === 0) {
+    let opts = [`--depth`, `1`];
+    if (typeof branch == `string`) {
+      opts.push(`--branch`, branch);
+    }
+    return Git().clone(remote, path, opts)
+  }
+  // If the directory is a git repo with the same remote, resolve
+  else if (await isAlreadyCloned(remote, path)) {
+    return true;
+  }
+  else {
+    throw new Error(`Can't clone to target destination: ${localPath}`);
+  }
+}
+
+
+exports.sourceNodes = ({ actions: {createNode}, store, createNodeId, reporter }, { name, remote, branch, patterns = `**` }) => {
+  const programDir = store.getState().program.directory;
+  const localPath = require('path').join(programDir, `.cache`, `gatsby-source-git`, name);
+
+  const createAndProcessNode = path => {
+    const fileNodePromise = createFileNode(path, createNodeId, { name: name, path: localPath }).then(fileNode => {
+      createNode(fileNode);
+      return null;
+    });
+    return fileNodePromise;
+  };
+
+  return getRepo(localPath, remote, branch)
+    .then(() => {
+      return fastGlob(patterns, { cwd: localPath, absolute: true });
+    })
+    .then((files) => {
+      return Promise.all(files.map(createAndProcessNode));
+    })
+    .catch(err => reporter.error(err));
+};
---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const fs = require(`fs-extra`);
+
+function loadNodeContent(fileNode) {
+  return fs.readFile(fileNode.absolutePath, `utf-8`);
+}
+
+exports.loadNodeContent = loadNodeContent;
